433ac694e5e1dfc8c5756236fa1279fa03919b87
ʀᴀʏ <nn_201312@163.com>
2023-03-10 04:44:40 +0000

    fix: metadata

--- test/foo.test.ts
+++ test/foo.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-import { foo } from "../packages/foo/src";
+import { foo } from "../packages/pkg-name/src";
 
 describe("foo", () => {
   it("should exported", () => {
