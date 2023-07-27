724202c60214f371e7c83837cee693407130b129
so1ve <nn_201312@163.com>
2023-07-27 17:42:52 +0800

    chore: format

--- test/foo.test.ts
+++ test/foo.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import { foo } from "../packages/pkg-name/src";
 
 describe("foo", () => {
-  it("should exported", () => {
-    expect(foo).toBe(1);
-  });
+	it("should exported", () => {
+		expect(foo).toBe(1);
+	});
 });
